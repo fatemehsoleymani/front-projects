c120e17ffacdbaeb7570763a18faef2d327960e2
fatemeh <fatemehsoleymani90@yahoo.com>
2025-02-14 15:08:18 -0500

    rename-emoji

--- rock-paper-scissors-project/rock-paper-scissors.js
+++ rock-paper-scissors-project/rock-paper-scissors.js
@@ -29,30 +29,30 @@ function autoPlay() {
 function playGame (playMove) {
 const computerMove = pickComputerMove();
 let result = '';
-if (playMove === 'Scissors') {
-if(computerMove === 'Rock') {
+if (playMove === 'scissors') {
+if(computerMove === 'rock') {
   result = 'You lose.'
- } else if (computerMove === 'Paper') {
+ } else if (computerMove === 'paper') {
   result = 'You win.'
- } else if (computerMove === 'Scissors') {
+ } else if (computerMove === 'scissors') {
   result = 'Tie'
   };  
 
-}else if (playMove === 'Paper') {
-if(computerMove === 'Rock') {
+}else if (playMove === 'paper') {
+if(computerMove === 'rock') {
 result = 'You win.'
-} else if (computerMove === 'Paper') {
+} else if (computerMove === 'paper') {
 result = 'Tie'
-} else if (computerMove === 'Scissors') {
+} else if (computerMove === 'scissors') {
 result = 'You lose.'
 };
 
-}else if (playMove === 'Rock') {
-if(computerMove === 'Rock') {
+}else if (playMove === 'rock') {
+if(computerMove === 'rock') {
 result = 'Tie'
-} else if (computerMove === 'Paper') {
+} else if (computerMove === 'paper') {
 result = 'You lose.'
-} else if (computerMove === 'Scissors') {
+} else if (computerMove === 'scissors') {
 result = 'You win.'
 };
 }
@@ -89,11 +89,11 @@ function pickComputerMove() {
 let computerMove = '';
 const randomNumber = Math.random();
 if (randomNumber >= 0 && randomNumber < 1/3) {
-computerMove = 'Rock';
+computerMove = 'rock';
   } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
-computerMove = 'Paper';
+computerMove = 'paper';
   } else if (randomNumber >= 2/3 && randomNumber < 1) {
-    computerMove = 'Scissors';
+    computerMove = 'scissors';
 }
 return computerMove;
 }
